84e6aeb49795faec2a42d45a8a6ce459ef91fb1e
Lucas Caetano <lucas.matheu.caetano@gmail.com>
2021-11-27 16:34:09 -0300

    dotenv e cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express  from "express";
 import { router } from "./routes";
+import cors from 'cors';
 
 const app = express ();
 
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors(options));
 app.use(express.json());
 app.use(router);
 
--- src/providers/implementations/MailgunMailProvider.ts
+++ src/providers/implementations/MailgunMailProvider.ts
@@ -2,13 +2,12 @@ import { IMailProvider, IMessage } from "../IMailProvider";
 import nodemailer from "nodemailer";
 import "dotenv/config";
 import mailgunTransport from "nodemailer-mailgun-transport";
-
 export class MailgunMailProvider implements IMailProvider{
     async sendMail(message: IMessage): Promise<void> {
         const auth = {
             auth: {
-              api_key: 'a187576a934d1a450c12ae6e18e6317e-7dcc6512-fe12117d',
-              domain: 'sandboxfc5413f331184bdfbf847efad4292c11.mailgun.org'
+              api_key: process.env.API_KEY,
+              domain: process.env.DOMAIN
             }
           }
         const nodemailerMailgun = nodemailer.createTransport(mailgunTransport(auth));
